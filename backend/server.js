8e414f2d98147b695dae5433608fdd970a6dbca5
Sathira Sri Sathara <sathira.nirmal@gmail.com>
2025-06-17 12:13:35 +0530

    feat: Update CORS configuration to use hardcoded allowed origins and change default port to 3080

--- backend/server.js
+++ backend/server.js
@@ -13,8 +13,14 @@ const db = require('./app/models');
 const userRoutes = require('./app/routes/user.routes');
 const authRoutes = require('./app/routes/auth.routes');
 
-// ✅ Setup CORS with .env origins
-const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(',') || [];
+
+// Allowed origins
+const allowedOrigins = [
+  'http://localhost:3000', // local development
+  'http://127.0.0.1:3000', // local development
+  'http://192.168.0.100:5173' // replace with your local IP address and port
+];
+
 
 app.use(cors({
   origin: (origin, callback) => {
@@ -43,7 +49,7 @@ app.get('/', (req, res) => {
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3080;
 app.listen(PORT, () => {
   console.log(`Server running --> http://localhost:${PORT}`);
 });
